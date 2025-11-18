7d4377efef8de1cbbb5ac2133972287721199720
jorge1sbr <jorge.p1998@hotmail.com>
2025-11-18 10:48:08 +0100

    Creación proyecto en SprigBoot

---
+++ Desktop/proyectoCRM/crm-cafe/src/main/java/com/ritualcoffee/crm/CrmCafeApplication.java
@@ -0,0 +1,13 @@
+package com.ritualcoffee.crm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrmCafeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrmCafeApplication.class, args);
+	}
+
+}
