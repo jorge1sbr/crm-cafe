f14c6c8266bfab2f8e5c38cb741875d634578854
jorge1sbr <jorge.p1998@hotmail.com>
2025-11-19 10:48:13 +0100

    creo el paquete dto y las clases registro y login request

---
+++ Desktop/proyectoCRM/crm-cafe/backend/src/main/java/com/ritualcoffee/crm/dto/LoginRequest.java
@@ -0,0 +1,24 @@
+package com.ritualcoffee.crm.dto;
+
+public class LoginRequest {
+
+    private String email;
+    private String password;
+    
+    // getters y setters
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+
+    
+}
